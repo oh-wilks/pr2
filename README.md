638692c95648ac4a076ae34113c77ed46eb18e58
NormanWilcox <wilcoxivan@hotmal.com>
2023-03-19 13:21:24 -0700

    modify read me

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pr2
-CRUD Application
+
+## CRUD APPLICATION 
+
+This application will store data in to a JSON object in the local storage. A JSON object needs to be stored as string , and to read it has to be parsed in to the JSON notation.
+
+## FUNCTIONALITY 
+
+
+### HTML 
+
+Html uses bootstrap for style management. 
---
+++ js/script.js
@@ -0,0 +1,46 @@
+const nombre = document.getElementById("nombreInput");
+const apellido = document.getElementById("apellidoInput");
+let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+const cuerpoTabla = document.getElementById("cuerpoTabla");
+
+const agregarUsuario = () => {
+  const usuario = {
+    id: crypto.randomUUID(),
+    nombre: nombre.value,
+    apellido: apellido.value,
+  };
+
+  usuarios.push(usuario);
+
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  mostrarUsuarios();
+};
+
+const mostrarUsuarios = () => {
+  cuerpoTabla.innerHTML = "";
+  usuarios.forEach((usuario) => {
+    cuerpoTabla.innerHTML += `<tr>
+        <th scope="row">${usuario.id}</th>
+        <td>${usuario.nombre}</td>
+        <td>${usuario.apellido}</td>
+        <td>
+        <button
+          type="button"
+          class="btn btn-danger"
+          onclick="eliminarUsuario('${usuario.id}')"
+        >
+          Eliminar
+        </button><td>
+    </tr>`;
+  });
+};
+
+const eliminarUsuario = (id) => {
+    const confirmacion = confirm("¿Está seguro de que desea eliminar este usuario?");
+    if (confirmacion) {
+      usuarios = usuarios.filter(usuario => usuario.id !== id);
+      localStorage.setItem("usuarios", JSON.stringify(usuarios));
+      mostrarUsuarios();
+    }
+  };
+  
